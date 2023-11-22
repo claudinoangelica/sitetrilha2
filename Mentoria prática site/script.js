19acd8b11cae68dc6605abd59c0d406ef507e62d
Angélica Claudino da Silva Gonçalves <127797863+claudinoangelica@users.noreply.github.com>
2023-11-22 01:14:24 -0300

    Add files via upload

---
+++ Mentoria prática site/script.js
@@ -0,0 +1,4 @@
+
+function carConteudo(){
+    document.getElementById("conteudo").innerHTML="O torneiro é uma das profissões mais antigas do mercado e mesmo assim continua sendo muito necessitada no dia a dia, principalmente por parte do setor automotivo e industrial.";
+}
